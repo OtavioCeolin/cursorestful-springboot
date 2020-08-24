655cc92293b226f28933260a48542f859562d699
OtavioCeolin <otavioceolin@gmail.com>
2020-08-24 20:17:02 -0300

    Primeiro commit

---
+++ src/main/java/com/example/cursorestfulspringboot/resources/ClienteResource.java
@@ -0,0 +1,21 @@
+package com.example.cursorestfulspringboot.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClienteResource {
+    
+    @GetMapping("/cliente")
+    public String getCliente(){
+
+        return "Vai retornar todos os clientes";
+    }
+
+    @GetMapping("/cliente/{codigo}")
+    public String getClienteByCodigo(@PathVariable int codigo){
+
+        return "Vai retornar o cliente de codigo: " + codigo;
+    }
+}
